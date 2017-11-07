76efe1adec6b1d7c34656005ba9dbcb15616fa49
Ogbonna Vitalis <agavitalisogbonna@gmail.com>
2017-11-07 01:58:01 +0100

    stsarted the student session

--- resources/views/partials/students/header.blade.php
+++ resources/views/partials/students/header.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Gentelella Alela! | </title>
+    <title>School Portal|Student </title>
 
     <!-- Bootstrap -->
     <link href="../css/bootstrap.min.css" rel="stylesheet">
