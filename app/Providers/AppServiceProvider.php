be91947ea824f935e88e96a9744206e7cf1570d2
Ogbonna <o.vitalis@genesystechhub.com>
2019-03-17 17:15:07 +0100

    Setting up deploys for heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,6 +15,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        //uncomment this when on a live server with https enabled
+        if(config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        } 
+
         //
         Schema::defaultStringLength(191);
     }
